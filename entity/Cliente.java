0561b7b21743ee93e30ddadf8101e9c2ad188a82
LAPTOP-8KBMGR26\evair <evairpsousa@gmail.com>
2017-12-06 21:57:06 -0200

    no message

--- entity/Cliente.java
+++ entity/Cliente.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "CLIENTE")
 public class Cliente  implements Serializable {
